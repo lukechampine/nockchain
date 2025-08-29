b74d9a1a3ef06dbd9d2cc2e0363d736fbb42a7e4
Aurimas Blažulionis <0x60@pm.me>
2025-08-29 11:05:06 +0100

    Add prom exporter to nbx-proxy build

--- nix/default.nix
+++ nix/default.nix
@@ -84,7 +84,7 @@ let
   ica // {
     pname = "nbx-miner";
     CARGO_PROFILE = profile;
-    cargoExtraArgs = "-p nbx-miner --bin nbx-proxy --features nbx-miner/jemalloc,nbx-miner/verifier,nbx-miner/force-preverify ${extraArgs}";
+    cargoExtraArgs = "-p nbx-miner --bin nbx-proxy --features nbx-miner/jemalloc,nbx-miner/verifier,nbx-miner/force-preverify,nbx-miner/prom-exporter ${extraArgs}";
     buildInputs = [ hoonc.hoonc ];
     preBuild = "mkdir -p assets && cp ${jam-pkg.verifier-jam.out} './assets/verifier.jam'";
   });
