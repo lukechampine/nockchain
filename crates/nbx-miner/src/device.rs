81f7a8a379a0a324a36f8a3b5193a8306511fa35
Aurimas Blažulionis <0x60@pm.me>
2025-09-22 12:23:21 +0100

    Remove machineid-rs, as it messes with GLIBC handling

--- crates/nbx-miner/src/device.rs
+++ crates/nbx-miner/src/device.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use bincode::{Decode, Encode};
-use machineid_rs::{Encryption, HWIDComponent, IdBuilder};
+use sha3::{Digest, Sha3_256};
 use sysinfo::{MemoryRefreshKind, System};
 
 use crate::proto::NAME_MAX_LENGTH;
@@ -17,11 +17,7 @@ pub struct DeviceInfo {
 }
 
 impl DeviceInfo {
-    pub fn new(client_name: Option<String>, is_proxy: bool) -> Self {
-        let mut sys = System::new();
-        sys.refresh_cpu_all();
-        sys.refresh_memory_specifics(MemoryRefreshKind::default().with_ram());
-
+    pub fn new(client_name: Option<String>, is_proxy: bool, sys: &System) -> Self {
         let hostname = System::host_name();
         let os_version = System::long_os_version().unwrap_or_else(|| "Unknown".to_string());
         let cpu_models = sys
@@ -50,19 +46,36 @@ pub struct Device {
 
 impl Device {
     pub fn new(client_name: Option<String>, is_proxy: bool) -> Self {
-        let key = client_name.as_deref().unwrap_or("hwid");
-        let hwid: Arc<str> = IdBuilder::new(Encryption::MD5)
-            .add_component(HWIDComponent::SystemID)
-            .add_component(HWIDComponent::CPUID)
-            .add_component(HWIDComponent::CPUCores)
-            .add_component(HWIDComponent::OSName)
-            .build(&format!("{is_proxy}-{key}"))
-            .expect("Unable to gather machine info")
-            .split_off(NAME_MAX_LENGTH)
-            .into();
+        let mut sys = System::new();
+        sys.refresh_cpu_all();
+        sys.refresh_memory_specifics(MemoryRefreshKind::default().with_ram());
 
-        let info = DeviceInfo::new(client_name, is_proxy);
+        let info = DeviceInfo::new(client_name.clone(), is_proxy, &sys);
+
+        let key = client_name.as_deref().unwrap_or("hwid");
+        let hwid: Arc<str> = hwid(&format!("{is_proxy}-{key}"), &info);
 
         Self { hwid, info }
     }
+}
+
+#[cfg(target_os = "linux")]
+fn machine_id() -> Option<String> {
+    for p in ["/var/lib/dbus/machine-id", "/etc/machine-id"] {
+        if let Ok(d) = std::fs::read_to_string(p) {
+            return Some(d);
+        }
+    }
+    None
+}
+
+fn hwid(key: &str, dev: &DeviceInfo) -> Arc<str> {
+    let mut state = Sha3_256::new();
+    state.update(&key);
+    state.update(&machine_id().unwrap());
+    state.update(&dev.os_version);
+    for cpu in &dev.cpu_models {
+        state.update(cpu);
+    }
+    format!("{:x}", state.finalize()).into()
 }
